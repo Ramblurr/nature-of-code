32b789d907defa9c593fd0c36745e350aafbd152
Casey Link <unnamedrambler@gmail.com>
2024-05-29 10:23:45 +0200

    Add stdev label to Exercise 0.4

--- src/noc/chapter_0_4e.cljs
+++ src/noc/chapter_0_4e.cljs
@@ -1,5 +1,7 @@
 (ns noc.chapter-0-4e
   (:require
+   [goog.string :as gstring]
+   [goog.string.format]
    [quil.core :as q]))
 
 (def size [640 240])
@@ -7,7 +9,7 @@
 (defn init-state [_]
   {:ui {:stdev {:type :slider
                 :min 5 :max 120
-                :value 10}}})
+                :value 50}}})
 
 (defn setup! [_]
   (q/color-mode :hsl 360)
@@ -24,4 +26,9 @@
         y (rand-gaus (quot height 2) stdev)]
     (q/no-stroke)
     (q/fill (rand-gaus 0 stdev) 200 200 200)
-    (q/ellipse x y 16 16)))
+    (q/ellipse x y 16 16)
+    (q/no-stroke)
+    (q/fill 360)
+    (q/rect 0 0 100 35)
+    (q/fill 0)
+    (q/text (gstring/format "stdev: %.2f" stdev) 10 20)))
